3db8522f7874091220cae2e3c999f190b2de4c32
Feeeeerran <oheydudeman@gmail.com>
2021-01-18 02:31:37 -0300

    Efectos + Remodel de la seccion de contacto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 // Contraccion del Nabar 
 const nav=document.getElementsByTagName("nav")[0];
 const navItems = document.querySelector(".sections").children;
-// const sections = document.getElementsByTagName("section")
+
+const sectionsTit=document.querySelectorAll("h3")
+const sectionsCont=document.querySelectorAll(".content")
 
 window.onload=()=>{
     for (var i=0;i<=4;i++) {
@@ -16,15 +18,23 @@ window.onload=()=>{
 window.addEventListener("scroll",()=> {
     if(window.scrollY>100) {
         nav.classList.add("pageOffset")
+        
 
     } else if (window.scrollY<50){
         nav.classList.remove("pageOffset")
     }
 
     for (var i=0;i<=4;i++) {
-        if (window.scrollY+50>window.innerHeight*(i)) {
+        if (window.scrollY+75>window.innerHeight*(i)) {
             RemoveActive();
             navItems[i].classList.add("active");
+            if (i<4) {
+                sectionsTit[i].classList.add("fadeIn-titulos");
+                sectionsCont[i].classList.add("fadeIn-content");
+                sectionsCont[i].classList.remove("content");
+            }
+            
+            
         }
     }
 })
@@ -34,3 +44,5 @@ function RemoveActive() {
         navItems[i].classList.remove("active");
     }
 }
+
+// Efectos
